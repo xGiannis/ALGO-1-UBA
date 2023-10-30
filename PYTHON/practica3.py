2840bb8ed57f242bb2b750e764e2b99417d38640
Gian Lucca Sanza <gianluccalord723@gmail.com>
2023-10-30 06:55:55 -0300

    falta repasar

--- PYTHON/practica3.py
+++ PYTHON/practica3.py
@@ -1,5 +1,7 @@
+from queue import Queue as Cola
 from queue import LifoQueue as Pila
- 
+import random
+
 
 #Ejercicio 1. Implementar en Python:
 
@@ -207,7 +209,6 @@ def sacarBarraN(texto:str):
             textoreemplazo += texto[i]
         else:
             break
-    print(textoreemplazo)
     return textoreemplazo
 
 
@@ -353,7 +354,363 @@ def promedioEstudiante(lu : str) -> float:
     print(promedio)
     return promedio
 
-promedioEstudiante("825/22")
+
+def promedioEstudiante2(lu):
+
+    archivo=open("notas.csv","r")
+
+    notasPosta=archivo.readlines()
+
+    notas:[list]=[]
+    #ESTA LINEA ME DESTRUYE PERO ES NECESARIA
+    for i in range(len(notasPosta)):
+        notareemplazo=notasPosta[i].split(",")
+        notareemplazo[3]=sacarBarraN(notareemplazo[3])
+        notas.append(notareemplazo)
+    #ESTA LINEA ME DESTRUYE PERO ES NECESARIA
+   
+    sumadenotas=0
+    cantnotas=0
+    
+
+    for nota in notas:
+        if nota[0]==lu:
+            cantnotas+=1
+            sumadenotas+=int(nota[3])
+
+
+    print(sumadenotas)
+    print(cantnotas)
+    if cantnotas!=0:
+
+        promedio=sumadenotas/cantnotas
+    else:
+        promedio=0
+    print(promedio)
+
+    return promedio 
+
+promedioEstudiante2("825/22")
+
+
+#PILAS
+#Ejercicio 8 ya lo hice creo
+def pilaOrdHasta(hasta:int):
+    pila=Pila()
+    for i in range(0,hasta,1):
+        pila.put(i)
+    return pila
+
+def pilaOrdHastaDesord(n,desde,hasta):
+    pila=Pila()
+    for i in range(n):
+
+        num=random.randint(desde,hasta)
+
+        pila.put(num)
+    
+    return pila
+
+
+
+"""Ejercicio 9. Implementar una funci´on cantidad elementos(in p : pila) → int que, dada una pila, cuente y devuelva la cantidad de elementos que contiene.
+ No se puede utilizar la funici´on LifoQueue.qsize(). Si se usa get() para recorrer la pila, esto
+modifica el par´ametro de entrada. Y como la especificaci´on dice que es de tipo in hay que restaurarla."""
+
+def cantidadElementos(p:Pila) ->int:
+    pinicial=p
+    lista=[]
+    size=0
+
+    
+    while not p.empty():
+        num=p.get()
+        print(num)
+        lista.append(num)
+        size+=1
+    for i in lista:
+        p.put(i)
+
+
+    print(p==pinicial)
+
+    print(size)
+    return size
+
+
+"""Ejercicio 10. Dada una pila de enteros, implementar una funci´on buscar el maximo(in p : pila) → int que devuelva el m´aximo
+elemento."""
+
+def buscarElMaximo(p:Pila)->int:
+    elementos=desarmarYarmarPila(p)
+    print(elementos)
+    for i in elementos:
+        esmasgrande=True
+
+        for j in elementos:
+            if i<j:
+                esmasgrande=False
+        
+        if esmasgrande:
+            mayor=i
+    print(mayor)
+    return mayor
+        
+
+
+#aux
+def desarmarYarmarPila(p:Pila)->list:
+    lista=[]
+    while not p.empty():
+        num=p.get()
+        lista.append(num)
+    for i in lista:
+        p.put(i)
+
+    return lista
+#aux
+
+
+
+"""Ejercicio 11. Implementar una funci´on esta bien balanceada(in s : str) → bool que dado un string con una formula aritm´etica sobre los enteros, 
+diga si los par´entesis estan bien balanceados. Las f´ormulas pueden formarse con:
+los numeros enteros
+las operaciones basicas +, −, x y /
+parentesis
+espacios
+Entonces las siguientes son formulas aritm´eticas con sus par´entesis bien balanceados:
+1 + ( 2 x 3 = ( 2 0 / 5 ) )
+10 * ( 1 + ( 2 * ( =1)))
+Y la siguiente es una formula que no tiene los par´entesis bien balanceados:
+1 + ) 2 x 3 ( ( )
+"""
+
+def estaBienBalanceada(s):
+    p=Pila()#PILA DE CARACTERES
+    for i in s:
+        p.put(i)
+
+    operadores= ["x","/","+","-","*"]
+    parentesisA=0
+    parentesisB=0
+
+    res=True
+    while not p.empty():
+        e=p.get()       #ABIEROTS NUNCA MAYOR QUE CERRADOS
+
+        print(e)
+        if e ==")":
+            parentesisB+=1
+        if e=="(":
+            parentesisA+=1
+
+
+        if parentesisA>parentesisB:
+            res=False
+
+        
+        #operadores
+        if e in operadores:
+            if operadoractivo:
+                res=False
+                print("Uso de 2 operadores seguidos")
+            else:
+                operadoractivo=True
+                
+        else:
+            operadoractivo=False
+        
+        #operadores
+
+        if e not in ["1","2","3","4","5","6","7","8","9"]:
+            if e not in operadores:
+                if e not in [" ","(",")"]:
+                    print("Uso de parametros no adecuados")
+                    res=False
+
+    print("parentesisA:",parentesisA)
+    print("parentesisB:",parentesisB)
+    if parentesisA==parentesisB and res==True:
+        res=True
+    else:
+        res=False
+    print(res)
+    return res
+
+
+"""Ejercicio 12. La notaci´on polaca inversa, tambi´en conocida como notaci´on postfix, es una forma de escribir expresiones matem´aticas
+ en la que los operadores siguen a sus operandos. Por ejemplo, la expresi´on “3 + 4” se escribe como “3 4 +” en notaci´on
+postfix. Para evaluar una expresi´on en notaci´on postfix, se puede usar una pila.
+Escribe una funci´on en Python que tome una expresi´on en notaci´on postfix y la eval´ue. La expresi´on estar´a representada
+como una cadena de caracteres, donde los operandos y operadores estar´an separados por espacios. Para simplificar el problema,
+s´olo vamos a considerar los operandos ‘+’, ‘-’, ‘*’ y ‘/’(suma, resta, multiplicaci´on y divisi´on), los operadores son n´umeros.
+Para resolver este problema, se recomienda seguir el siguiente algoritmo:
+1. Dividir la expresi´on en tokens (operandos y operadores) utilizando espacios como delimitadores.
+2. Recorre los tokens uno por uno.
+a) Si es un operando, agr´egalo a una pila.
+b) Si es un operador, saca los dos operandos superiores de la pila, apl´ıcale el operador y luego coloca el resultado en la
+pila.
+3. Al final de la evaluaci´on, la pila contendr´a el resultado de la expresi´on.
+Ejemplo de uso:
+expresion = "3 4 + 5 * 2 -"
+resultado = evaluar_expresion(expresion)
+print(resultado) # Deber´ıa devolver 33"""
+
+
+def evaluar_postfix(string:str)->int:
+    p=Pila()
+
+    ecuacion=string.split(" ")
+    print(ecuacion)
+    operadores=["+","-","/","*"]
+    for i in ecuacion:
+        if i in operadores:
+            xD=int(p.get())
+            xI=int(p.get())
+            if i == "+":
+                calc=xI+xD
+            elif i == "-":
+                calc=xI-xD
+            elif i =="/":
+                calc=xI/xD
+            elif i =="*":
+                calc=xI*xD
+            
+            p.put(str(calc))
+
+        else:
+            p.put(i)
+
+    res=p.get()
+    print(res)
+    return res
+
+
+
+
+
+#COLAS
+
+"""Ejercicio 13. Usando la funci´on generar nros al azar() definida en la secci´on anterior, implementar una funci´on que arme
+una cola de enteros con los numeros generados al azar. """
+def colaAlAzar(n,desde,hasta):
+    cola=Cola()
+    for i in range(n):
+        num=random.randint(desde,hasta)
+        cola.put(num)
+    return cola
+
+"""
+Ejercicio 14. Implementar una funci´on cantidad elementos(in c : cola) → int que, dada una cola, cuente y devuelva la
+cantidad de elementos que contiene. Comparar con la versi´on usando pila. No se puede utilizar la funci´on Queue.qsize()."""
+
+def cantidadElementosCOLA(c:Cola):
+    cinicial=c
+    lista=[]
+    size=0
+
+    
+    while not c.empty():
+        num=c.get()
+        print(num)
+        lista.append(num)
+        size+=1
+    for i in lista:
+        c.put(i)
+
+
+    print(c==cinicial)
+
+    print(size)
+    return size   
+
+
+"""Ejercicio 15. Dada una cola de enteros, implementar una funci´on buscar el maximo(in c : cola) → int que devuelva el m´aximo
+elemento. Comparar con la versi´on usando pila.
+"""
+def maximoCola(c:Cola)->int:
+    elementos=desarmarYarmarCola(c)
+    print(elementos)
+    for i in elementos:
+        esmasgrande=True
+
+        for j in elementos:
+            if i<j:
+                esmasgrande=False
+        
+        if esmasgrande:
+            mayor=i
+    print(mayor)
+    return mayor
+
+#aux
+def desarmarYarmarCola(c:Cola)->list:
+    lista=[]
+    while not c.empty():
+        num=c.get()
+        lista.append(num)
+    for i in lista:
+        c.put(i)
+
+    return lista
+#aux
+
+"""Ejercicio 16. Bingo: un cart´on de bingo contiene 12 n´umeros al azar en el rango [0, 99].
+1. implementar una funci´on armar secuencia de bingo() → Cola[int] que genere una cola con los n´umeros del 0 al 99
+ordenados al azar.
+2. implementar una funci´on jugar carton de bingo(in carton : list[int], in bolillero : cola[int]) → int que toma un
+cart´on de Bingo y una cola de enteros (que corresponden a las bolillas numeradas) y determina cual es la cantidad de
+jugadas de ese bolillero que se necesitan para ganar.
+"""
+
+
+def secuenciaBingo():
+    cola=Cola()
+    numeros=[]
+    a=0
+    for i in range(0,100): #PRUEBO CON 10 PRIMERO
+        num=random.randint(0,99)
+        while num in numeros:
+            num=random.randint(0,99) #TAL VEZ ESTO TARDE AÑOS
+        numeros.append(num)
+        a=a+1
+
+
+
+        cola.put(num)
+
+    print(a)
+    print("")
+    return cola
+
+print("\n\n\n")
+
+
+def jugarCartonBingo(carton:list[int],bolillero:Cola[int]): #carton tiene 15 numeros
+    jugada=0
+    print(len(carton))
+    fichascarton=15
+    
+    while fichascarton!=0:
+        
+        bola=bolillero.get()
+        print(bola)
+        jugada+=1
+        print(bola in carton)
+        if bola in carton:
+            fichascarton=fichascarton-1
+    print(jugada)
+    return jugada
+        
+
+
+
+"""Ejercicio 17. Vamos a modelar una guardia de un hospital usando una cola donde se van almacenando los pedidos de atenci´on
+para los pacientes que van llegando. A cada paciente se le asigna una prioridad del 1 al 10 (donde la prioridad 1 es la mas urgente
+y requiere atenci´on inmediata) junto con su nombre y la especialidad medica que le corresponde.
+Implementar la funci´on n pacientes urgentes(in c : Cola[(int, str, str)]) → int que devuelve la cantidad de pacientes de
+la cola que tienen prioridad en el rango [1, 3]."""
+#PONGO EN PAUSA COLA PARA REPASAR BIBLIOTECAS
 
 
 
@@ -361,9 +718,37 @@ promedioEstudiante("825/22")
 
 
 
+#4. Diccionarios
+
+"""Ejercicio 20. Volver a implementar la funci´on que calcula el promedio de las notas de los alumnos, pero ahora devolver un
+diccionario {libreta universitaria : promedio} con los promedios de todos los alumnos.
+"""
 
 
 
+def promedioEstudiante2LIBRETAPOSTA():
+
+    archivo1=open("notas.csv","r")
+
+    notasPosta=archivo1.readlines()
 
 
 
+    lus:[list]=[]
+    #ESTA LINEA ME DESTRUYE PERO ES NECESARIA
+    for i in range(len(notasPosta)):
+        notareemplazo=notasPosta[i].split(",")
+        if notareemplazo not in lus:
+
+            lus.append(notareemplazo[0])
+    #ESTA LINEA ME DESTRUYE PERO ES NECESARIA
+   
+    d={}
+    
+    for lu in lus:
+        d[lu]=promedioEstudiante2(lu)
+
+    print(d)
+    return d
+        
+promedioEstudiante2LIBRETAPOSTA()
--- PYTHON/testin3.py
+++ PYTHON/testin3.py
@@ -2,19 +2,7 @@ from practica3 import *
 from queue import LifoQueue as Pila
 import random
 
-def pilaOrdHasta(hasta:int):
-    pila=Pila()
-    for i in range(0,hasta,1):
-        pila.put(i)
-    return pila
 
-def pilaOrdHastaDesord(hasta:int):
-    pila=Pila()
-    for i in range(hasta):
-        num=random.randint(0,10)
-
-        pila.put(num)
-    return pila
 
 p=Pila()
 p.put(1)
@@ -26,8 +14,6 @@ p.put(4)
 sampleText="textoEjemplo.txt"
 textomodificable="textoejemplo2.txt"
 
-pilaOrd=pilaOrdHasta(10)
-pilaDesord=pilaOrdHastaDesord(10)
 
 #buscarElMaximo(pilaOrd)
 #buscarElMaximo(pilaDesord)
@@ -40,8 +26,55 @@ pilaDesord=pilaOrdHastaDesord(10)
 
 #agregarFrase(textomodificable,"Las abuelas de plaza de mayo\nExijen libertad!")
 
-agregarFrasePrincipio(textomodificable,"In the beggining...\n")
+#agregarFrasePrincipio(textomodificable,"In the beggining...\n")
 
 
+#promedioEstudiante2("825/22")
 
+
+#PILAS¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+print("")
+print("")
+print("")
+
+pilaOrd=pilaOrdHasta(10)
+pilaDesord=pilaOrdHastaDesord(10,0,20)
+#cantidadElementos(pilaDesord)
+
+#buscarElMaximo(pilaDesord)
+
+"""
+estaBienBalanceada("2 + 2")
+print("")
+estaBienBalanceada("3*5+(22)/4)")
+estaBienBalanceada("((2x33)/32)")
+"""
+
+#evaluar_postfix("2 2 + 10 * 2 -")
+
+
+#COLA¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+
+colaDesord=colaAlAzar(10,0,20)
+
+#cantidadElementos(colaDesord)
+#print("")
+#maximoCola(colaDesord)
+
+
+def hacerCarton():
+    carton=[]
+    for i in range(15):
+        num=random.randint(0,99)
+        while num in carton:
+            num=random.randint(0,99)
+        carton.append(num)
+    print(carton)
+    return carton
+
+
+carton=hacerCarton()
+bolillero=secuenciaBingo()
+
+jugarCartonBingo(carton,bolillero)
 
